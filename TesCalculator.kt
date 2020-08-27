4ed29710cc8dbbe4a8487bf79c642d88415c4531
tejesh77026 <45883728+tejesh77026@users.noreply.github.com>
2020-08-27 22:25:15 +0530

    few edits

--- TesCalculator.kt
+++ TesCalculator.kt
@@ -1,15 +1,14 @@
 import java.util.*
 
 fun main(){
-    println("i am kotlin")
     val reader = Scanner(System.`in`)
-    print("Enter two numbers: ")
+    print("Enter any two numbers: ")
 
-    // nextDouble() reads the next double from the keyboard
+    // to read double values from the keyboard
     val first = reader.nextDouble()
     val second = reader.nextDouble()
 
-    print("Enter an operator (+, -, *, /): ")
+    print("Enter any operator (+, -, *, /): ")
 
     val operator = reader.next()[0]
 
@@ -20,9 +19,9 @@ fun main(){
         '-' -> result = first - second
         '*' -> result = first * second
         '/' -> result = first / second
-        // operator doesn't match any case constant (+, -, *, /)
+        // when operator doesn't match with(+, -, *, /)
         else -> {
-            System.out.printf("Error! operator is not correct")
+            System.out.printf("Enter correct operator")
             return
         }
     }
